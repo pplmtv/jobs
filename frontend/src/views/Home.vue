b4949380ec3b4295abe807a7386d4f5d6d4548e1
pplmtv <pplmtv@gmail.com>
2021-10-09 10:01:57 +0900

    logo.svg plugins updated

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-container>
+            <h1>Job Board</h1>
+            <div v-for="job in jobs" :key="job.pk">
+                <p>{{ job.company_name }}：{{ job.job_title }}</p>
+            </div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import { apiService } from "../common/api.service.js"
+export default {
+    name: "home",
+    data() {
+        return {
+            jobs: []
+        }
+    },
+    methods: {
+        getJobs() {
+            let endpoint = "api/jobs/"
+            apiService(endpoint).then(data => {
+                this.jobs.push(...data.results)
+            })
+        }
+    },
+    created() {
+        this.getJobs()
+        console.log(this.jobs)
+    },
+};
+</script>
