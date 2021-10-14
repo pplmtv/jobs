0b7b4da99ea653b17cd0e35bd63ddeb61a4d6ca4
pplmtv <pplmtv@gmail.com>
2021-10-15 06:45:39 +0900

    Job.vue updated

--- frontend/src/views/Job.vue
+++ frontend/src/views/Job.vue
@@ -13,6 +13,11 @@
           :to="{ name: 'editor', params: { id: job.id } }"
       >編集
       </v-btn>
+      <v-btn
+          color="error"
+          @click="deleteJobData"
+      >削除
+      </v-btn>
     </v-container>
   </div>
 </template>
@@ -44,7 +49,15 @@ export default {
       apiService(endpoint).then(data => {
         this.job = data;
         this.setPageTitle(data.company_name);
-      })
+      });
+    },
+    deleteJobData() {
+      let endpoint = `/api/jobs/${this.id}/`;
+      apiService(endpoint,"DELETE").then(() => {
+        this.$router.push({
+          name: 'home'
+        });
+      });
     }
   },
   created() {
